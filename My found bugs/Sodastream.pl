27f6bb61ed25930146a23dbb3f93cccbc0ff8829
Patryk Dąbrowski <98545181+swempix@users.noreply.github.com>
2022-10-08 20:53:37 +0200

    Update Sodastream.pl

--- My found bugs/Sodastream.pl
+++ My found bugs/Sodastream.pl
@@ -1,1 +1,16 @@
+Sodastream.pl 18.09.2022
 
+System: Windows 10
+Web browser: Google Chrome
+Bug place: Register Express Page
+Bug type: Visual
+Bug name: After entering wrong data subtitles in fields are not centred
+
+1. Go to https://sodastream.pl/
+2. Click 'Zarejestruj swój ekspres'
+3. Scroll down
+4. Click 'Zatwierdź'
+
+Expected result: After entering wrong data, words in fields are centred as before entering any data.
+
+Acutal result: When the wrong data is entered, the words in the fields are not centred correctly.
