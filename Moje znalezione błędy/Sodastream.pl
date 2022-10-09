b98542ed17aafdfe32646f6fa5fd44e1411aaabe
Patryk Dąbrowski <98545181+dabrowskipatryk@users.noreply.github.com>
2022-10-09 13:22:20 +0200

    Create Sodastream.pl

---
+++ Moje znalezione błędy/Sodastream.pl
@@ -0,0 +1,17 @@
+Sodastream.pl     18.09.2022
+
+System: Windows 10
+Przeglądarka: Google Chrome
+Miejsce błędu: Strona 'Zarejestruj swój ekspres'
+Typ błędu: Wizualny
+Nazwa błędu: Po wprowadzeniu błędnych danych napisy w polach nie są wyśrodkowane
+
+Kroki by zreprodukować błąd:
+1. Otwórz https://sodastream.pl/
+2. Kliknij 'Zarejestruj swój ekspres'
+3. Zjedź w dół
+4. Kliknij 'Zatwierdź'
+
+Oczekiwany rezultat: Po wprowadzeniu błędnych danych słowa w polach są wyśrodkowane jak przed wpisaniem danych.
+
+Aktualny rezultat: Po wprowadzeniu błędnych danych słowa w polach nie są prawidłowo wyśrodkowane.
