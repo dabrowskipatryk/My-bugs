a999c4810619ec35801c63e579e08802dcd40598
Patryk Dąbrowski <98545181+dabrowskipatryk@users.noreply.github.com>
2022-12-20 19:34:31 +0100

    Create Decathlon.pl

---
+++ Moje znalezione błędy/Decathlon.pl
@@ -0,0 +1,25 @@
+
+Decathlon.pl       Data: 07.12.2022
+
+System: Windows 10
+Przeglądarka: Opera, Google Chrome
+Miejsce błędu: Koszyk
+Typ błędu: Funkcjonalny
+Nazwa błędu: Brak możliwości dodania adresu podczas finalizacji zakupu
+
+Dane wstępne:
+Użytkownik posiada konto na stronie Decathlon.pl, jest na nim zalogowany oraz nie ma dodanego adresu w ustawieniach konta.
+
+Kroki by zreprodukować błąd:
+1. Wejdź na stronę decathlon.pl
+2. Wyszukaj produkt od innego dostawcy niż Decathlon (Przykładowy produkt: https://www.decathlon.pl/p/mp/kubota/japonki-meskie-plazowe-kubota-classic-czarne/_/R-p-7f02036d-6b2c-4d55-a73e-7edfc363b7a8?mc=7f02036d-6b2c-4d55-a73e-7edfc363b7a8_c8)
+3. Wybierz rozmiar (Przykładowo 44)
+4. Kliknij 'Dodaj do koszyka'
+5. Kliknij 'Idź do koszyka'
+6. Kliknij 'Przejdź do dostawy'
+7. Wypełnij wszystkie wymagane pola poniżej Opcji 'Dostawa do domu' (Oznaczone '*')
+8. Kliknij 'Przejdź do płatności'
+
+Oczekiwany rezultat: Adres dostawy zostanie zapisany oraz Użytkownik zostanie przeniesiony do wyboru formy płatności.
+
+Aktualny rezultat: Adres dostawy nie zostaje zapisany i zostaje zwrócony błąd o nie wprowadzeniu żadengo adresu dostawy.
